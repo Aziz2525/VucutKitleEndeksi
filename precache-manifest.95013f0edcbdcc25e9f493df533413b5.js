217431b11f2e8ba281a79253c44fbd181ba6e785
Aziz2525 <aziz.app.developer@gmail.com>
2020-01-27 12:47:38 +0300

    Updates

---
+++ precache-manifest.95013f0edcbdcc25e9f493df533413b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cfd43f9a3ed6728f8bb0bac283144d5",
+    "url": "/VucutKitleEndeksi/index.html"
+  },
+  {
+    "revision": "816b0e39021f2acd8306",
+    "url": "/VucutKitleEndeksi/static/css/main.61f5f102.chunk.css"
+  },
+  {
+    "revision": "3f98718f49ec054f1706",
+    "url": "/VucutKitleEndeksi/static/js/2.cd38e1da.chunk.js"
+  },
+  {
+    "revision": "8a14dcea7ad8b593c1e83c11f7ba1a89",
+    "url": "/VucutKitleEndeksi/static/js/2.cd38e1da.chunk.js.LICENSE"
+  },
+  {
+    "revision": "816b0e39021f2acd8306",
+    "url": "/VucutKitleEndeksi/static/js/main.08baa7cf.chunk.js"
+  },
+  {
+    "revision": "646633aeb571a3b586cd",
+    "url": "/VucutKitleEndeksi/static/js/runtime-main.6f4ea126.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/VucutKitleEndeksi/static/media/logo.5d5d9eef.svg"
+  }
+]);
